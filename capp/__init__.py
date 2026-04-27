ebf34843d781f873814830c45bb62d117b2bbeb8
SabrinAdan <93946082+SabrinAdan@users.noreply.github.com>
2026-04-27 21:51:37 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -15,9 +15,16 @@ application = Flask(__name__)
 application.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','dev-secret-key')
 #application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 #application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+if 'RDS_DB_NAME' in os.environ:
+    DBVAR = f"postgresql://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOSTNAME']}:{os.environ['RDS_PORT']}/{os.environ['RDS_DB_NAME']}"
+    application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR
+    application.config['SQLALCHEMY_BINDS'] = {'transport': DBVAR}
+else:
+    application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 
 db.init_app(application)
